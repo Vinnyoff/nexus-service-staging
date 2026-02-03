624e7519e4f60d59fdfd95a670ba4338c7e3d245
Marcos Vinicius Freitas <marcosvf2000@gmail.com>
2026-02-03 12:14:50 +0000

    Add android capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.incodedev.nexus', // TROQUE
+  appName: 'Nexus',                 // TROQUE
+  webDir: 'public',                 // placeholder (qualquer pasta existente)
+  server: {
+    url: 'https://studio--studio-7906776988-e180b.us-central1.hosted.app',
+    cleartext: false
+  }
+};
+
+export default config;
